f0118a4fa1c6d89099b1f5e49833178d0a48f19b
marcocas <marcocas@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2011-11-09 15:04:16 +0000

    Fixed directory removal

--- create_zip.bat
+++ create_zip.bat
@@ -8,10 +8,10 @@ call %BASE%\buildwin.bat
 set /P PROG_VER=< %BASE%\src\version.inc
 set PROG_VER=%PROG_VER:~1,-1%
 set BIN_DIR=%BASE%\bin\win32
-set ZIPSRCDIR=%BASE%\zip\win32
+set ZIPSRCDIR=%BASE%\packages\zip\win32
 set PACKAGES_DIR=%BASE%\packages
 
-del /s /q %ZIPSRCDIR% > nul
+rd /s /q %ZIPSRCDIR% > nul
 MKDIR %ZIPSRCDIR%
 MKDIR %ZIPSRCDIR%\locale
 MKDIR %ZIPSRCDIR%\resources		
@@ -22,7 +22,10 @@ copy /Y  %BASE%\images\nocover.png %ZIPSRCDIR%\resources
 copy /Y  %BASE%\images\volume-slider.png %ZIPSRCDIR%\resources
 copy /Y  %BASE%\images\volume-slider-mask.png %ZIPSRCDIR%\resources		
 del /q  %PACKAGES_DIR%\ovoplayer-%PROG_VER%-%FULL_TARGET%.zip 
-zip -9 -r  %PACKAGES_DIR%\ovoplayer-%PROG_VER%-%FULL_TARGET%.zip %ZIPSRCDIR%
-DEL /S /Q %ZIPSRCDIR% > nul
+cd /d %ZIPSRCDIR%
+zip -9 -r  %PACKAGES_DIR%\ovoplayer-%PROG_VER%-%FULL_TARGET%.zip .
+cd /d %BASE%
+rd /S /Q %ZIPSRCDIR% > nul
+
 
 endlocal
