b5084ec9f351fb330772240e51303f98eed74ecd
marcocas@gmail.com <marcocas@gmail.com@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2012-01-09 18:30:31 +0000

    Bump version to 0.0.3 Fix error in desktop file Add debian changelog file Better description for debian package

--- create_deb.sh
+++ create_deb.sh
@@ -35,12 +35,15 @@ $INSTALLFILE  $BASE/images/logo.png $DEBSRCDIR/usr/share/pixmaps/ovoplayer.png
 $INSTALLFILE  $BASE/images/nocover.png $DEBSRCDIR/usr/share/ovoplayer
 $INSTALLFILE  $BASE/images/volume-slider.png $DEBSRCDIR/usr/share/ovoplayer
 $INSTALLFILE  $BASE/images/volume-slider-mask.png $DEBSRCDIR/usr/share/ovoplayer
-$INSTALLFILE  $BASE/packages/ovoplayer.desktop $DEBSRCDIR/usr/share/applications
+$INSTALLFILE  $PACKAGES_DIR/ovoplayer.desktop $DEBSRCDIR/usr/share/applications
 
-$INSTALLFILE  $BASE/packages/copyright $DEBSRCDIR/usr/share/doc/ovoplayer
+$INSTALLFILE  $PACKAGES_DIR/copyright $DEBSRCDIR/usr/share/doc/ovoplayer
 
 gzip -c --best $BASE/DOC/manpages/ovoplayer.1 > $DEBSRCDIR/usr/share/man/man1/ovoplayer.1.gz
 chmod 0644 $DEBSRCDIR/usr/share/man/man1/ovoplayer.1.gz
+
+gzip -c --best $PACKAGES_DIR/changelog > $DEBSRCDIR/usr/share/doc/ovoplayer/changelog.gz
+chmod 0644 $DEBSRCDIR/usr/share/doc/ovoplayer/changelog.gz
 
 INSTALLEDSIZE=$(du -xs --apparent-size --block-size=1024 $DEBSRCDIR/usr | sed -e '$s/[^0-9]//g')
 sed -e 's/:INSTALLEDSIZE/'$INSTALLEDSIZE'/;s/:VERSION/'$PROG_VER'/' $BASE/packages/control > $DEBSRCDIR/DEBIAN/control
@@ -50,7 +53,7 @@ cd $DEBSRCDIR
 find usr -type f | xargs md5sum >> $DEBSRCDIR/DEBIAN/md5sums
 chmod 0644 $DEBSRCDIR/DEBIAN/md5sums
 
-fakeroot dpkg --build $DEBSRCDIR $PACKAGES_DIR/ovoplayer-$PROG_VER-x86_64.deb
+fakeroot dpkg --build $DEBSRCDIR $PACKAGES_DIR/ovoplayer-$PROG_VER-gtk2-amd64.deb
 
 
 
