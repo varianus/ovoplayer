0eb775bfb1dba7cac8102f12f5a4527fe8b858f6
marcocas <marcocas@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2012-02-29 09:56:26 +0000

    Updated tag reader example applications

--- src/ovotag/examples/unit1.pas
+++ src/ovotag/examples/unit1.pas
@@ -1,4 +1,4 @@
-unit Unit1; 
+unit Unit1;
 
 {$mode objfpc}{$H+}
 
@@ -6,23 +6,23 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, Spin, basetag;
+  ExtCtrls, Spin, AudioTag, basetag;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    Button4: TButton;
-    Button5: TButton;
+    bOpenFile: TButton;
+    bSave: TButton;
+    bSave1: TButton;
     edAlbum: TEdit;
     edAlbumArtist: TEdit;
     edArtist: TEdit;
     edGenre: TEdit;
     edTitle: TEdit;
+    gbCommonTags: TGroupBox;
+    gbMediaInfo: TGroupBox;
     Image1: TImage;
     Label1: TLabel;
     Label10: TLabel;
@@ -49,17 +49,25 @@ type
     meComment: TMemo;
     Memo1: TMemo;
     OpenDialog1: TOpenDialog;
+    Panel1: TPanel;
+    SaveDialog1: TSaveDialog;
     seTrack: TSpinEdit;
     seYear: TSpinEdit;
+    procedure bSaveAsClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure ReadTag(Sender: TObject);
   private
-    { private declarations }
+    TagReader: TTagReader;
+    CommonTags: TCommonTags;
+    procedure MapToTags;
+    procedure MediaInfoToMap;
+    procedure TagsToMap;
   public
     { public declarations }
-  end; 
+  end;
 
 var
-  Form1: TForm1; 
+  Form1: TForm1;
 
 implementation
 
@@ -70,62 +78,111 @@ uses file_flac, file_wma, file_mp3, file_ogg, file_monkey;
 
 procedure TForm1.ReadTag(Sender: TObject);
 var
-  i: Integer;
-  TagReader: TTagReader;
-  CommonTags: TCommonTags;
-  FileStream: TfileStream;
+  i: integer;
+  tagClass: TTagReaderClass;
   tmpsr: string;
 begin
-  memo1.clear;
-  if not OpenDialog1.Execute then exit;
+  memo1.Clear;
 
-  case (Sender as TButton).Tag of
-    1: TagReader:= TFlacReader.Create;
-    2: TagReader:= TWMAReader.Create;
-    3: TagReader:= TMP3Reader.Create;
-    4: TagReader:= TOGGReader.Create;
-    5: TagReader:= TMonkeyReader.Create;
-  end;
+  if not OpenDialog1.Execute then
+    exit;
+
+  if Assigned(TagReader) then
+    TagReader.Free;
+
+  tagClass := IdentifyKind(OpenDialog1.FileName);
+  TagReader := tagClass.Create;
 
   TagReader.LoadFromFile(OpenDialog1.FileName);
-  CommonTags:= TagReader.GetCommonTags;
-  for i:= 0 to TagReader.Tags.Count -1 do
-    begin
-      tmpsr := TagReader.Tags.Frames[i].ID+ ' ---> '+  TagReader.Tags.Frames[i].AsString;
-      Memo1.Lines.Add( tmpsr ) ;
-    end;
+  CommonTags := TagReader.GetCommonTags;
+  for i := 0 to TagReader.Tags.Count - 1 do
+  begin
+    tmpsr := TagReader.Tags.Frames[i].ID + ' ---> ' + TagReader.Tags.Frames[i].AsString;
+    Memo1.Lines.Add(tmpsr);
+  end;
 
- leFileName.Caption :=CommonTags.FileName ;
- leDuration.Caption := TimeToStr(CommonTags.Duration / MSecsPerDay) ;
- leBitRate.Caption := format('%d Kbps',[TagReader.MediaProperty.BitRate]);
- leBPM.Caption := IntTostr(TagReader.MediaProperty.BPM);
- leChannels.Caption := TagReader.MediaProperty.ChannelMode;
- leSampling.Caption := format('%d Hz',[TagReader.MediaProperty.Sampling]);
+  MediaInfoToMap;
+  TagsToMap;
 
- leFileName.Caption := CommonTags.FileName;
- edArtist.Caption := CommonTags.Artist;
- edAlbum.Caption := CommonTags.Album;
- edAlbumArtist.Caption := CommonTags.AlbumArtist;
- edGenre.Caption := CommonTags.Genre;
- edTitle.Caption := CommonTags.Title;
- i:=0;
- TryStrToInt(CommonTags.Year,i);
- seYear.Value := i;
+end;
 
- seTrack.Value := CommonTags.Track;
+procedure TForm1.MediaInfoToMap;
+begin
+  leDuration.Caption := TimeToStr(CommonTags.Duration / MSecsPerDay);
+  leBitRate.Caption := format('%d Kbps', [TagReader.MediaProperty.BitRate]);
+  leBPM.Caption := IntToStr(TagReader.MediaProperty.BPM);
+  leChannels.Caption := TagReader.MediaProperty.ChannelMode;
+  leSampling.Caption := format('%d Hz', [TagReader.MediaProperty.Sampling]);
 
- if TagReader.Tags.ImageCount > 0 then
-   begin
-      TagReader.Tags.Images[0].Image.Position:= 0;
-      FileStream:= TFileStream.Create('c:\xxxxx.jpg', fmCreate);
-      FileStream.CopyFrom(TagReader.Tags.Images[0].Image, TagReader.Tags.Images[0].Image.Size);
-      TagReader.Tags.Images[0].Image.Position:= 0;
-      Image1.Picture.LoadFromStream(TagReader.Tags.Images[0].Image);
-      FileStream.Free;
-   end;
+end;
 
-  TagReader.Free;
+procedure TForm1.MapToTags;
+var
+  i: integer;
+  ImageStream: TMemoryStream;
+begin
+  CommonTags.FileName    := leFileName.Caption;
+  CommonTags.Artist      := edArtist.Caption;
+  CommonTags.Album       := edAlbum.Caption;
+  CommonTags.AlbumArtist := edAlbumArtist.Caption;
+  CommonTags.Genre       := edGenre.Caption;
+  CommonTags.Title       := edTitle.Caption;
+  CommonTags.Year        := IntTostr(seYear.Value);
+  CommonTags.Track       := seTrack.Value;
+end;
+
+procedure TForm1.TagsToMap;
+var
+  i: integer;
+  ImageStream: TMemoryStream;
+begin
+  leFileName.Caption := CommonTags.FileName;
+  edArtist.Caption := CommonTags.Artist;
+  edAlbum.Caption := CommonTags.Album;
+  edAlbumArtist.Caption := CommonTags.AlbumArtist;
+  edGenre.Caption := CommonTags.Genre;
+  edTitle.Caption := CommonTags.Title;
+  i := 0;
+  TryStrToInt(CommonTags.Year, i);
+  seYear.Value := i;
+
+  seTrack.Value := CommonTags.Track;
+
+  if TagReader.Tags.ImageCount > 0 then
+  begin
+    TagReader.Tags.Images[0].Image.Position := 0;
+    ImageStream := TMemoryStream.Create;
+    ImageStream.CopyFrom(TagReader.Tags.Images[0].Image,
+      TagReader.Tags.Images[0].Image.Size);
+    TagReader.Tags.Images[0].Image.Position := 0;
+    Image1.Picture.LoadFromStream(TagReader.Tags.Images[0].Image);
+    ImageStream.Free;
+  end;
+
+end;
+
+procedure TForm1.bSaveAsClick(Sender: TObject);
+var
+  i: integer;
+begin
+  if TagReader = nil then
+    exit;
+  SaveDialog1.FileName := ExtractFileNameOnly(TagReader.GetCommonTags.FileName) +
+                          '.new' +
+                          ExtractFileExt(TagReader.GetCommonTags.FileName);
+
+  if not SaveDialog1.Execute then
+    exit;
+
+  MapToTags;
+  TagReader.SetCommonTags(CommonTags);
+  TagReader.SaveToFile(SaveDialog1.FileName);
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  TagReader := nil;
 end;
 
 end.
-
