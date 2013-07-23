b31863b8e7f7e7bf8a3f4f0798fc4d336abaa6e0
marcocas@gmail.com <marcocas@gmail.com@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2013-07-23 14:23:20 +0000

    Move extended song info class in a dedicated unit

---
+++ src/extendedinfo.pas
@@ -0,0 +1,41 @@
+unit ExtendedInfo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  { TExtendedInfo }
+
+  TExtendedInfo = Class
+    Id: Integer;
+    PlayCount : Integer;
+    Rating :Integer;
+    tmpRating: integer;
+    Added : TDateTime;
+    LastPlay : TDateTime;
+  public
+    constructor Create;
+  end;
+
+
+implementation
+
+{ TExtendedInfo }
+
+constructor TExtendedInfo.Create;
+begin
+   PlayCount := -1;
+   Rating := -1;
+   tmpRating := -1;
+   Added := 0;
+   LastPlay := 0;
+
+end;
+
+
+end.
+
