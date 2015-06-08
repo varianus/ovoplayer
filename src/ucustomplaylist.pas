a2b11ffad9195c2f223e7f8853594e0e268b6129
varianus <varianus@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2015-06-08 14:51:14 +0000

    Custom playlist generator: simple support for limits and random order

--- src/ucustomplaylist.pas
+++ src/ucustomplaylist.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  ButtonPanel, Buttons, ufrfield, fgl, PlaylistBuilder, GUIBackEnd;
+  ButtonPanel, Buttons, StdCtrls, Spin, ufrfield, fgl, PlaylistBuilder,
+  GUIBackEnd;
 
 type
 
@@ -16,9 +17,12 @@ type
   TfCustomPlayList = class(TForm)
     bPlus: TBitBtn;
     ButtonPanel1: TButtonPanel;
+    ckRandom: TCheckBox;
+    ckLimit: TCheckBox;
     Panel1: TPanel;
     Panel2: TPanel;
     sbFieldContainer: TScrollBox;
+    seLimits: TSpinEdit;
     procedure bPlusClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure OKButtonClick(Sender: TObject);
@@ -42,19 +46,28 @@ procedure TfCustomPlayList.FormCreate(Sender: TObject);
 begin
   Fields := TFieldContainer.Create(true);
   AddField;
+//  seLimits.AnchorToNeighbour(akLeft, 10, ckLimit);
 end;
 
 procedure TfCustomPlayList.OKButtonClick(Sender: TObject);
 var
-  filters: string;
+  filters, sorts: string;
   i: integer;
 begin
   Filters:= '1=1'; // dummy test
   for i := 0 to Fields.Count -1 do
     Filters := Filters + Fields[i].GetFilter;
 
+  if ckRandom.Checked then
+    Sorts := ' random() '
+  else
+    Sorts := ' id ';
+  if ckLimit.Checked then
+    sorts:= sorts + ' Limit ' + inttostr(seLimits.Value);
+
+
   BackEnd.Manager.ImportFromMediaLibrary(BackEnd.mediaLibrary, BackEnd.PlayList,
-        Filters, ' random()');
+        Filters, sorts );
 
 end;
 
