ee72dc8bda34497411aedf11ff3f1a3342f153fe
varianus <varianus@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2017-05-16 14:29:18 +0000

    Add some code to test equalizer

--- src/uequalizer.pas
+++ src/uequalizer.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
-  ExtCtrls, StdCtrls, equalizerband, fgl;
+  ExtCtrls, StdCtrls, equalizerband, fgl, AudioEngine;
 
 type
 
@@ -16,14 +16,17 @@ type
   TfEqualizer = class(TForm)
     ButtonPanel1: TButtonPanel;
     cbEnableEq: TCheckBox;
+    lbMessage: TLabel;
     pnlHeader: TPanel;
     pnlContainer: TPanel;
+    procedure cbEnableEqClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
   private
+    Eq: IEqualizer;
     EQBandList: TEQBandList;
     procedure BandChanged(Sender: TObject; const BandNo:integer; const Value: single);
   public
-
 
   end;
 
@@ -31,6 +34,7 @@ var
   fEqualizer: TfEqualizer;
 
 implementation
+uses GUIBackEnd;
 
 {$R *.lfm}
 
@@ -43,12 +47,23 @@ procedure TfEqualizer.FormCreate(Sender: TObject);
 var
   i:integer;
   EQBand: TfrEqualizer;
+  BandInfo: ARBandinfo;
 begin
 
+  if not  Supports(BackEnd.AudioEngine, IEqualizer, Eq) then
+    begin
+      lbMessage.Caption:='UNSUPPORTED ENGINE';
+      pnlContainer.Enabled:=false;
+      pnlHeader.Enabled:=false;
+      exit;
+    end;
+
+  eq.ActiveEQ:=true;
+  BandInfo := Eq.BandInfo;
   EQBandList:= TEQBandList.Create(false);
 
   pnlContainer.DisableAlign;
-  for i := 1 to EQCounter do
+  for i := 1 to length(BandInfo) -1 do
   begin
     EQBand:= TfrEqualizer.Create(pnlContainer);
     EQBandList.Add(EQBand);
@@ -57,17 +72,29 @@ begin
     EQBand.OnBandChanged:=@BandChanged;
     EQBand.Align := alLeft;
     EQBand.Left := i* EQBand.Width;
-    EQBand.InitBand(i,inttostr(i*10), i, -12,+12);
+    EQBand.InitBand(i, FloatToStr(BandInfo[i].freq), BandInfo[i-1].Value, -12,+12);
   end;
   pnlContainer.EnableAlign;
 
 
 end;
 
+procedure TfEqualizer.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+    eq.ActiveEQ:=false;
+end;
+
+procedure TfEqualizer.cbEnableEqClick(Sender: TObject);
+begin
+  Eq.ActiveEQ:=cbEnableEq.Checked;
+
+end;
+
 procedure TfEqualizer.BandChanged(Sender: TObject; const BandNo: integer;
   const Value: single);
 begin
-
+  eq.BandValue[BandNo]:= Value;
+  eq.EQApply;
 end;
 
 end.
