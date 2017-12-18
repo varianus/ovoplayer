5348d9e7a8b828b4f5869289e2964b8b6aa501a8
varianus <varianus@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2017-12-18 16:46:29 +0000

    [MERGE from branch 1.3] Add script to build executables with debug info enabled

---
+++ buildlinux_dbg.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# set -x
+#Set path where lazarus is installed
+LAZARUS_DIR=~/development/lazarus
+CONFIG_PATH=
+# 
+if [ "$BASE" = "" ]; then
+  BASE=$(pwd)
+fi   
+   
+if [ ! "$OS_TARGET" = "" ]; then 
+  DC_ARCH="$DC_ARCH --os=$OS_TARGET"
+fi
+   
+if [ ! "$CPU_TARGET" = "" ]; then
+ DC_ARCH="$DC_ARCH --cpu=$CPU_TARGET"
+fi 
+
+if [ ! "$WIDGETSET_TARGET" = "" ]; then
+ DC_ARCH="$DC_ARCH --cpu=$WIDGETSET_TARGET"
+fi 
+
+NONE=-l
+if [ ! "$CONFIG_PATH" = "" ]; then
+ PCP=--pcp="$CONFIG_PATH"
+fi
+
+# clean build files
+rm -Rf $BASE/src/lib/*
+rm -Rf $BASE/src/components/lib/*.*
+rm -Rf $BASE/tools/ovoplayerctrl/lib/*.*
+
+rm -Rf $BASE/bin/linux
+#
+cp $BASE/debug.cfg  $BASE/extrafpc.cfg
+$LAZARUS_DIR/lazbuild -B -r $PCP --build-mode=Release $BASE/src/components/mcaselli.lpk $DC_ARCH
+$LAZARUS_DIR/lazbuild -B -r $PCP --build-mode=Release $BASE/src/ovoplayer.lpi $DC_ARCH
+$LAZARUS_DIR/lazbuild -B -r $PCP --build-mode=Release $BASE/tools/ovoplayerctrl/ovoplayerctrl.lpi $DC_ARCH
+#strip --strip-all $BASE/bin/linux/ovoplayer
+#strip --strip-all $BASE/bin/linux/ovoplayerctrl
+
+echo $NONE > $BASE/extrafpc.cfg
