058d2d0a717c8ede9842c0937436c3fdd0bac3dd
varianus <varianus@7d61162a-b7fa-4814-bc65-dfb66743d53e>
2018-11-26 09:02:23 +0000

    Calculate duration for Monkey audio file (*.ape)

--- src/ovotag/file_monkey.pas
+++ src/ovotag/file_monkey.pas
@@ -26,7 +26,7 @@ uses
   Classes, lazutf8classes, SysUtils, AudioTag, baseTag, tag_APE;
 
 const
-  MonkeyFileMask: string    = '*.ape;';
+  MonkeyFileMask: string = '*.ape;';
 
 type
 
@@ -50,6 +50,57 @@ implementation
 
 uses tag_id3v2;
 
+type
+  { Real structure of Monkey's Audio header }
+  // common header for all versions
+  APE_HEADER = packed record
+    cID: array[0..3] of byte;        // should equal 'MAC '
+    nVersion: word;                 // version number * 1000 (3.81 = 3810)
+  end;
+  // old header for <= 3.97
+  APE_HEADER_OLD = packed record
+    nCompressionLevel,               // the compression level
+    nFormatFlags,                    // any format flags (for future use)
+    nChannels: word;                 // the number of channels (1 or 2)
+    nSampleRate,                     // the sample rate (typically 44100)
+    nHeaderBytes,                    // the bytes after the MAC header that compose the WAV header
+    nTerminatingBytes,               // the bytes after that raw data (for extended info)
+    nTotalFrames,                    // the number of frames in the file
+    nFinalFrameBlocks: longword;     // the number of samples in the final frame
+    nInt: integer;
+  end;
+  // new header for >= 3.98
+  APE_HEADER_NEW = packed record
+    nCompressionLevel: word;       // the compression level (see defines I.E. COMPRESSION_LEVEL_FAST)
+    nFormatFlags: word;      // any format flags (for future use) Note: NOT the same flags as the old header!
+    nBlocksPerFrame: longword;    // the number of audio blocks in one frame
+    nFinalFrameBlocks: longword;    // the number of audio blocks in the final frame
+    nTotalFrames: longword;    // the total number of frames
+    nBitsPerSample: word;      // the bits per sample (typically 16)
+    nChannels: word;      // the number of channels (1 or 2)
+    nSampleRate: longword;    // the sample rate (typically 44100)
+  end;
+  // data descriptor for >= 3.98
+  APE_DESCRIPTOR = packed record
+    padded: word;                   // padding/reserved (always empty)
+    nDescriptorBytes,                // the number of descriptor bytes (allows later expansion of this header)
+    nHeaderBytes,                  // the number of header APE_HEADER bytes
+    nSeekTableBytes,                // the number of bytes of the seek table
+    nHeaderDataBytes,                // the number of header data bytes (from original file)
+    nAPEFrameDataBytes,             // the number of bytes of APE frame data
+    nAPEFrameDataBytesHigh,           // the high order number of APE frame data bytes
+    nTerminatingDataBytes: longword;  // the terminating data of the file (not including tag data)
+    cFileMD5: array[0..15] of byte;  // the MD5 hash of the file (see notes for usage... it's a littly tricky)
+  end;
+
+Const   { Compression level codes }
+   //MONKEY_COMPRESSION_FAST       = 1000;  { Fast (poor) }
+   //MONKEY_COMPRESSION_NORMAL     = 2000;  { Normal (good) }
+   //MONKEY_COMPRESSION_HIGH       = 3000;  { High (very good) }
+   MONKEY_COMPRESSION_EXTRA_HIGH = 4000;  { Extra high (best) }
+   //MONKEY_COMPRESSION_INSANE     = 5000;  { Insane }
+   //MONKEY_COMPRESSION_BRAINDEAD = 6000; { BrainDead }
+
 { TMonkeyReader }
 
 function TMonkeyReader.GetDuration: int64;
@@ -71,14 +122,20 @@ end;
 
 function TMonkeyReader.isUpdateable: boolean;
 begin
-  Result:=true;
+  Result := True;
 end;
 
 function TMonkeyReader.LoadFromFile(AFileName: Tfilename): boolean;
 var
   fStream: TFileStreamUTF8;
-  tempTags : TTags;
-  HaveID3V2 :boolean;
+  tempTags: TTags;
+  HaveID3V2: boolean;
+  BlocksPerFrame: Integer;
+
+  APE: APE_HEADER;     // common header
+  APE_OLD: APE_HEADER_OLD; // old header   <= 3.97
+  APE_NEW: APE_HEADER_NEW; // new header   >= 3.98
+  APE_DESC: APE_DESCRIPTOR; // extra header >= 3.98
 
 begin
   Result := inherited LoadFromFile(AFileName);
@@ -86,14 +143,73 @@ begin
   try
     tempTags := TID3Tags.Create;
     HaveID3V2 := tempTags.ReadFromStream(fStream);
-    fStream.Position:=0;
+    fStream.Position := 0;
     ftags := TAPETags.Create;
     Result := ftags.ReadFromStream(fStream);
-    if (not result) and HaveID3V2 then
-       begin
-         fTags.free;
-         fTags := tempTags;
-       end;
+    if (not Result) and HaveID3V2 then
+    begin
+      fTags.Free;
+      fTags := tempTags;
+    end;
+    if HaveID3V2 then
+      fStream.Seek(TID3Tags(tempTags).Size, soFromBeginning)
+    else
+      fStream.Seek(0, soFromBeginning);
+
+    FillByte(APE, sizeof(APE), 0);
+    if (fStream.Read(APE, sizeof(APE)) = sizeof(APE)) and (StrLComp(@APE.cID[0], 'MAC ', 4) = 0) then
+    begin
+      if APE.nVersion >= 3980 then
+      begin
+        FillByte(APE_DESC, sizeof(APE_DESC), 0);
+        if (fStream.Read(APE_DESC, sizeof(APE_DESC)) = sizeof(APE_DESC)) then
+        begin
+          // seek past description header
+          if APE_DESC.nDescriptorBytes <> 52 then
+            fStream.Seek(APE_DESC.nDescriptorBytes - 52, soFromCurrent);
+          // load new ape_header
+          if APE_DESC.nHeaderBytes > sizeof(APE_NEW) then
+            APE_DESC.nHeaderBytes := sizeof(APE_NEW);
+          fillchar(APE_NEW, sizeof(APE_NEW), 0);
+          if (longword(fStream.Read(APE_NEW, APE_DESC.nHeaderBytes)) = APE_DESC.nHeaderBytes) then
+          begin
+            // based on MAC SDK 3.98a1 (APEinfo.h)
+            FSampleRate := APE_NEW.nSampleRate;
+            if APE_NEW.nTotalFrames > 0 then
+            begin
+              FSamples :=
+                int64(APE_NEW.nBlocksPerFrame) * int64(APE_NEW.nTotalFrames - 1) +
+                int64(APE_NEW.nFinalFrameBlocks);
+            end;
+          end;
+        end;
+      end
+      else
+      begin
+        // Old Monkey <= 3.97
+        fillchar(APE_OLD, sizeof(APE_OLD), 0);
+        if (fStream.Read(APE_OLD, sizeof(APE_OLD)) = sizeof(APE_OLD)) then
+        begin
+          FSampleRate := APE_OLD.nSampleRate;
+          // based on MAC_SDK_397 (APEinfo.cpp)
+          if (APE.nVersion >= 3950) then
+            BlocksPerFrame := 73728 * 4
+          else if (APE.nVersion >= 3900) or ((APE.nVersion >= 3800) and
+            (APE_OLD.nCompressionLevel = MONKEY_COMPRESSION_EXTRA_HIGH)) then
+            BlocksPerFrame := 73728
+          else
+            BlocksPerFrame := 9216;
+          // calculate total uncompressed samples
+          if APE_OLD.nTotalFrames > 0 then
+          begin
+            FSamples :=
+              int64(APE_OLD.nTotalFrames - 1) * int64(BlocksPerFrame) +
+              int64(APE_OLD.nFinalFrameBlocks);
+          end;
+        end;
+      end;
+
+    end;
   finally
     fstream.Free;
   end;
@@ -104,45 +220,44 @@ function TMonkeyReader.SaveToFile(AFileName: Tfilename): boolean;
 var
   SourceStream: TFileStreamUTF8;
   DestStream: TFileStreamUTF8;
-  v1rec : TID3V1Record;
-  offset : cardinal;
-  header : TAPEHeader;
+  v1rec: TID3V1Record;
+  offset: cardinal;
+  header: TAPEHeader;
 begin
-  Result:=inherited SaveToFile(AFileName);
+  Result := inherited SaveToFile(AFileName);
 
   SourceStream := TFileStreamUTF8.Create(FileName, fmOpenRead or fmShareDenyNone);
   DestStream := TFileStreamUTF8.Create(AFileName, fmCreate or fmOpenReadWrite or fmShareDenyNone);
 
   try
     SourceStream.Seek(SourceStream.Size - SizeOf(TID3V1Record), soFromBeginning);
-    SourceStream.Read(V1Rec,  SizeOf(TID3V1Record));
+    SourceStream.Read(V1Rec, SizeOf(TID3V1Record));
 
     if V1Rec.Header <> 'TAG' then
-       Offset := 0
+      Offset := 0
     else
-       Offset := SizeOf(V1Rec);
+      Offset := SizeOf(V1Rec);
 
     SourceStream.Seek(SourceStream.Size - SizeOf(TAPEHeader) - offset, soFromBeginning);
     SourceStream.Read(Header, sizeof(TAPEHeader));
 
-    if String(Header.Marker) = APE_IDENTIFIER then
-      begin
-        Offset := offset + header.TagSize;
-        SourceStream.Seek(SourceStream.Size - SizeOf(Header) - offset, soFromBeginning);
-        SourceStream.Read(Header, sizeof(Header));
-        if String(Header.Marker) = APE_IDENTIFIER then
-           Offset := offset + sizeof(Header);
-      end;
+    if string(Header.Marker) = APE_IDENTIFIER then
+    begin
+      Offset := offset + header.TagSize;
+      SourceStream.Seek(SourceStream.Size - SizeOf(Header) - offset, soFromBeginning);
+      SourceStream.Read(Header, sizeof(Header));
+      if string(Header.Marker) = APE_IDENTIFIER then
+        Offset := offset + sizeof(Header);
+    end;
 
-   SourceStream.Position := 0;
-   DestStream.CopyFrom(SourceStream, SourceStream.Size - offset);
-   Tags.WriteToStream(DestStream);
-   Result := True;
+    SourceStream.Position := 0;
+    DestStream.CopyFrom(SourceStream, SourceStream.Size - offset);
+    Tags.WriteToStream(DestStream);
+    Result := True;
   finally
     SourceStream.Free;
     DestStream.Free;
   end;
-
 
 end;
 
@@ -150,4 +265,3 @@ initialization
   RegisterTagReader(MonkeyFileMask, TMonkeyReader);
 
 end.
-
