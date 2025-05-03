a575e85659eab550ba0ab5c7ff8a04f0dbc04429
bbrtj <bbrtj.pro@gmail.com>
2025-05-03 18:52:29 +0200

    Fix breaking files by saving empty tags

--- src/ovotag/tag_id3v2.pas
+++ src/ovotag/tag_id3v2.pas
@@ -341,6 +341,13 @@ begin
   header.Flags := 0;
   tmpSize := 0;
 
+  // Remove empty frames
+  for i := Count - 1 downto 0 do
+  begin
+    if Frames[i].Size = 0 then
+      Remove(Frames[i]);
+  end;
+
   for i := 0 to Count - 1 do
   begin
     tmpSize := tmpSize + Frames[i].Size + HeadSize;
