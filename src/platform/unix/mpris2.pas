2ca206d8cbce1b95c2f8d668548937ec017f6524
varianus <marcocas@gmail.com>
2026-08-13 11:31:29 +0200

    Add missing comma

--- src/platform/unix/mpris2.pas
+++ src/platform/unix/mpris2.pas
@@ -326,7 +326,7 @@ begin
         if (dbus_message_get_args(message_, @error, DBUS_TYPE_STRING, [@s_val, DBUS_TYPE_INVALID])) > 0 then
         begin
           if (strlen(s_val) > 0) then
-            BackEnd.HandleExternalCommand(BuildCommand(CATEGORY_APP,COMMAND_ENQUEUE, StrPas(s_val));
+            BackEnd.HandleExternalCommand(BuildCommand(CATEGORY_APP,COMMAND_ENQUEUE, StrPas(s_val)));
         end
         else
           dbus_error_free(@error);
