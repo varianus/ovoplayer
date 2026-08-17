8ebb4ee7a827256cd897c1fa1a10e4a8bfb1a336
varianus <marcocas@gmail.com>
2026-08-17 13:41:27 +0200

    Always paint equalizer bands Use diffrents colors if themed slider is disables

--- src/components/themedslider.pas
+++ src/components/themedslider.pas
@@ -15,34 +15,35 @@ type
 
   TThemedSlider = class(TCustomControl)
   private
-    FMax: Cardinal;
-    FMin: Cardinal;
+    FMax: cardinal;
+    FMin: cardinal;
     FOnChange: TNotifyEvent;
     FOrientation: TThemedSliderOrientation;
-    FPosition: Cardinal;
-    fSeeking : Boolean;
-    FSliderwidth: Integer;
+    FPosition: cardinal;
+    fSeeking: boolean;
+    FSliderwidth: integer;
     FStep: cardinal;
     InternalBitmap: TBitmap;
     procedure RedrawControl;
-    procedure Seek(X, Y: Integer);
-    procedure SetMax(const AValue: Cardinal);
-    procedure SetMin(const AValue: Cardinal);
+    procedure Seek(X, Y: integer);
+    procedure SetMax(const AValue: cardinal);
+    procedure SetMin(const AValue: cardinal);
     procedure SetOrientation(AValue: TThemedSliderOrientation);
-    procedure SetPosition(const AValue: Cardinal);
-    procedure SetSliderwidth(const AValue: Integer);
+    procedure SetPosition(const AValue: cardinal);
+    procedure SetSliderwidth(const AValue: integer);
     procedure SetStep(const AValue: cardinal);
     { Private declarations }
   protected
-    Procedure Paint; Override;
+    procedure Paint; override;
     procedure SetBounds(ALeft, ATop, AWidth, AHeight: integer); override;
-    Function  DoMouseWheel(Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint): Boolean; override;
-    procedure MouseMove(Shift: TShiftState; X,Y: Integer); override;
-    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);  override;
-    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    function DoMouseWheel(Shift: TShiftState; WheelDelta: integer; MousePos: TPoint): boolean; override;
+    procedure MouseMove(Shift: TShiftState; X, Y: integer); override;
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: integer); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: integer); override;
     procedure Click; override;
+    procedure Repaint; override;
   public
-    Constructor Create(AOwner: TComponent); override;
+    constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
     procedure EraseBackground(DC: HDC); override;
 
@@ -52,12 +53,12 @@ type
     property BorderWidth;
     property Enabled;
     property Step: cardinal read FStep write SetStep;
-    Property Max: Cardinal read FMax write SetMax;
-    Property Min: Cardinal read FMin write SetMin;
-    Property Position: Cardinal read FPosition write SetPosition;
-    Property Sliderwidth: Integer read FSliderwidth write SetSliderwidth;
-    Property OnChange: TNotifyEvent read FOnChange write FOnChange;
-    property Orientation : TThemedSliderOrientation read FOrientation write SetOrientation;
+    property Max: cardinal read FMax write SetMax;
+    property Min: cardinal read FMin write SetMin;
+    property Position: cardinal read FPosition write SetPosition;
+    property Sliderwidth: integer read FSliderwidth write SetSliderwidth;
+    property OnChange: TNotifyEvent read FOnChange write FOnChange;
+    property Orientation: TThemedSliderOrientation read FOrientation write SetOrientation;
     property OnClick;
 
   end;
@@ -66,24 +67,24 @@ implementation
 
 { TThemedSlider }
 
-procedure TThemedSlider.SetMax(const AValue: Cardinal);
+procedure TThemedSlider.SetMax(const AValue: cardinal);
 begin
-  if FMax=AValue then exit;
-  FMax:=AValue;
+  if FMax = AValue then exit;
+  FMax := AValue;
   RedrawControl;
 end;
 
-procedure TThemedSlider.SetMin(const AValue: Cardinal);
+procedure TThemedSlider.SetMin(const AValue: cardinal);
 begin
-  if FMin=AValue then exit;
-  FMin:=AValue;
+  if FMin = AValue then exit;
+  FMin := AValue;
   RedrawControl;
 end;
 
 procedure TThemedSlider.SetOrientation(AValue: TThemedSliderOrientation);
 var
-  OldWidth: LongInt;
-  OldHeight: LongInt;
+  OldWidth: longint;
+  OldHeight: longint;
 begin
   if FOrientation <> AValue then
   begin
@@ -92,34 +93,34 @@ begin
     // lfm contains a consistent combination of Orientation and (width, height)
     if not (csLoading in ComponentState) then
     begin
-      OldWidth:=Width;
-      OldHeight:=Height;
-      SetBounds(Left,Top,OldHeight,OldWidth);
+      OldWidth  := Width;
+      OldHeight := Height;
+      SetBounds(Left, Top, OldHeight, OldWidth);
     end;
   end;
 end;
 
 
-procedure TThemedSlider.SetPosition(const AValue: Cardinal);
+procedure TThemedSlider.SetPosition(const AValue: cardinal);
 begin
   if fSeeking then
-     exit;
-  if FPosition=AValue then exit;
-  FPosition:=AValue;
+    exit;
+  if FPosition = AValue then exit;
+  FPosition := AValue;
   RedrawControl;
 end;
 
-procedure TThemedSlider.SetSliderwidth(const AValue: Integer);
+procedure TThemedSlider.SetSliderwidth(const AValue: integer);
 begin
-  if FSliderwidth=AValue then exit;
-  FSliderwidth:=AValue;
+  if FSliderwidth = AValue then exit;
+  FSliderwidth := AValue;
   RedrawControl;
 end;
 
 procedure TThemedSlider.SetStep(const AValue: cardinal);
 begin
-  if FStep=AValue then exit;
-  FStep:=AValue;
+  if FStep = AValue then exit;
+  FStep := AValue;
 end;
 
 procedure TThemedSlider.RedrawControl;
@@ -127,82 +128,90 @@ var
   RectInnerArea: TRect;
   RectSlider: TRect;
   RectFilled: TRect;
-  pos, invpos :integer;
-  HalfSlider:Integer;
+  pos, invpos: integer;
+  HalfSlider: integer;
   SlidingDimension: integer;
-//  Color : DWORD;
+  //  Color : DWORD;
 const
-  FrameWidth=1;
+  FrameWidth = 1;
 begin
   HalfSlider := FSliderwidth div 2;
 
-  InternalBitmap.canvas.Brush.Color:= WidgetSet.GetSysColor(COLOR_BTNFACE);
+  InternalBitmap.canvas.Brush.Color := WidgetSet.GetSysColor(COLOR_BTNFACE);
   InternalBitmap.Canvas.FillRect(0, 0, InternalBitmap.Width, InternalBitmap.Height);
 
-  InternalBitmap.Transparent:=true;
-  InternalBitmap.TransparentMode:=tmAuto;
-  RectInnerArea:=rect(BorderWidth, BorderWidth, Width - BorderWidth, Height - BorderWidth);
+  InternalBitmap.Transparent := True;
+  InternalBitmap.TransparentMode := tmAuto;
+  RectInnerArea := rect(BorderWidth, BorderWidth, Width - BorderWidth, Height - BorderWidth);
   WidgetSet.Frame3d(InternalBitmap.canvas.Handle, RectInnerArea, FrameWidth, bvLowered);
   RectInnerArea.Inflate(-FrameWidth, -FrameWidth);
 
 
   case FOrientation of
     ttsHorizontal: SlidingDimension := Width;
-    ttsVertical: SlidingDimension := Height;
+    ttsVertical: SlidingDimension   := Height;
   end;
 
   if fMax = 0 then
-    pos:=0
+    pos := 0
   else
-    pos:= trunc(SlidingDimension * (FPosition / (fMax - fMin)));
+    pos := trunc(SlidingDimension * (FPosition / (fMax - fMin)));
 
   if pos < HalfSlider then
-      pos := HalfSlider;
+    pos := HalfSlider;
 
   if pos > SlidingDimension - HalfSlider then
-    pos:= SlidingDimension - HalfSlider;
+    pos := SlidingDimension - HalfSlider;
 
   if FOrientation = ttsVertical then
-     invpos := Height - pos;
+    invpos := Height - pos;
 
   case FOrientation of
-       ttsHorizontal: RectFilled:=Rect(RectInnerArea.Left , RectInnerArea.Top, pos - HalfSlider, RectInnerArea.Bottom);
-       ttsVertical  : RectFilled:=Rect(RectInnerArea.left , RectInnerArea.Bottom, RectInnerArea.Right, invpos + HalfSlider);
+    ttsHorizontal: RectFilled := Rect(RectInnerArea.Left, RectInnerArea.Top, pos - HalfSlider, RectInnerArea.Bottom);
+    ttsVertical: RectFilled   := Rect(RectInnerArea.left, RectInnerArea.Bottom, RectInnerArea.Right, invpos + HalfSlider);
   end;
 
-   if pos > HalfSlider then
-     begin
-        InternalBitmap.canvas.Brush.Color:= WidgetSet.GetSysColor(COLOR_HIGHLIGHT);
-        InternalBitmap.Canvas.FillRect(RectFilled);
-     end;
+  if pos > HalfSlider then
+  begin
+    if IsEnabled then
+      InternalBitmap.canvas.Brush.Color := WidgetSet.GetSysColor(COLOR_HIGHLIGHT)
+    else
+      InternalBitmap.canvas.Brush.Color := WidgetSet.GetSysColor(COLOR_WINDOW);
+
+    InternalBitmap.Canvas.FillRect(RectFilled);
+  end;
 
 
   case FOrientation of
-    ttsHorizontal: RectFilled:=Rect(pos + HalfSlider, RectInnerArea.Top, RectInnerArea.Right , RectInnerArea.Bottom);
-    ttsVertical  : RectFilled:=Rect(RectInnerArea.left, RectInnerArea.top,  RectInnerArea.Right, RectInnerArea.top + invpos - HalfSlider);
+    ttsHorizontal: RectFilled := Rect(pos + HalfSlider, RectInnerArea.Top, RectInnerArea.Right, RectInnerArea.Bottom);
+    ttsVertical: RectFilled   := Rect(RectInnerArea.left, RectInnerArea.top, RectInnerArea.Right, RectInnerArea.top + invpos - HalfSlider);
   end;
 
   if pos < SlidingDimension - SLIDERWIDTH then
-    begin
-       InternalBitmap.canvas.Brush.Color:= WidgetSet.GetSysColor(COLOR_WINDOW);
-       InternalBitmap.Canvas.FillRect(RectFilled);
-    end;
+  begin
+    InternalBitmap.canvas.Brush.Color := WidgetSet.GetSysColor(COLOR_WINDOW);
+    InternalBitmap.Canvas.FillRect(RectFilled);
+  end;
 
   case FOrientation of
-    ttsHorizontal: RectSlider:=Rect(pos - HalfSlider, BorderWidth div 2, pos + HalfSlider,  Height- (BorderWidth div 2));
-    ttsVertical  : RectSlider:=Rect(BorderWidth div 2, invpos - HalfSlider,  Width- (BorderWidth div 2),  invpos + HalfSlider);
+    ttsHorizontal: RectSlider := Rect(pos - HalfSlider, BorderWidth div 2, pos + HalfSlider, Height - (BorderWidth div 2));
+    ttsVertical: RectSlider   := Rect(BorderWidth div 2, invpos - HalfSlider, Width - (BorderWidth div 2), invpos + HalfSlider);
   end;
 
   if fSeeking then
-    begin
-      WidgetSet.DrawFrameControl(InternalBitmap.canvas.Handle, RectSlider, DFC_BUTTON, DFCS_PUSHED+DFCS_BUTTONPUSH);
-      WidgetSet.DrawEdge(InternalBitmap.canvas.Handle, RectSlider, EDGE_SUNKEN, BF_RECT+ BF_MONO);
-    end
+  begin
+    WidgetSet.DrawFrameControl(InternalBitmap.canvas.Handle, RectSlider, DFC_BUTTON, DFCS_PUSHED + DFCS_BUTTONPUSH);
+    WidgetSet.DrawEdge(InternalBitmap.canvas.Handle, RectSlider, EDGE_SUNKEN, BF_RECT + BF_MONO);
+  end
   else
-    begin
-      WidgetSet.DrawFrameControl(InternalBitmap.canvas.Handle, RectSlider, DFC_BUTTON, DFCS_BUTTONPUSH);
-      WidgetSet.DrawEdge(InternalBitmap.canvas.Handle, RectSlider, EDGE_Raised, BF_RECT+ BF_mono);
-    end;
+  begin
+    if IsEnabled then
+      WidgetSet.DrawFrameControl(InternalBitmap.canvas.Handle, RectSlider, DFC_BUTTON, DFCS_BUTTONPUSH)
+    else
+      WidgetSet.DrawFrameControl(InternalBitmap.canvas.Handle, RectSlider, DFC_BUTTON, DFCS_BUTTONPUSH + DFCS_INACTIVE);
+
+    WidgetSet.DrawEdge(InternalBitmap.canvas.Handle, RectSlider, EDGE_Raised, BF_RECT + BF_mono);
+  end;
 
   Invalidate;
 end;
@@ -217,101 +226,101 @@ procedure TThemedSlider.SetBounds(ALeft, ATop, AWidth, AHeight: integer);
 begin
   inherited SetBounds(ALeft, ATop, AWidth, AHeight);
   if InternalBitmap = nil then
-     exit;
-  InternalBitmap.Width:=AWidth;
-  InternalBitmap.Height:=AHeight;
+    exit;
+  InternalBitmap.Width  := AWidth;
+  InternalBitmap.Height := AHeight;
   RedrawControl;
 end;
 
-function TThemedSlider.DoMouseWheel(Shift: TShiftState; WheelDelta: Integer;
-  MousePos: TPoint): Boolean;
+function TThemedSlider.DoMouseWheel(Shift: TShiftState; WheelDelta: integer;
+  MousePos: TPoint): boolean;
 begin
 
   if not IsEnabled then
     exit;
 
   if WheelDelta > 0 then
-     Position:= Position + FStep
+    Position := Position + FStep
   else
-     Position:= Position - FStep;
+    Position := Position - FStep;
 
   if Assigned(FOnChange) then
-     FOnChange(self);
+    FOnChange(self);
 
-  Result:=inherited DoMouseWheel(Shift, WheelDelta, MousePos);
+  Result := inherited DoMouseWheel(Shift, WheelDelta, MousePos);
 
 end;
 
-procedure TThemedSlider.Seek(X, Y: Integer);
+procedure TThemedSlider.Seek(X, Y: integer);
 var
-  tmppos :Cardinal;
+  tmppos: cardinal;
   SlidingDimension: integer;
   Refer: integer;
 begin
 
   case FOrientation of
-      ttsHorizontal: begin
-                       SlidingDimension := Width;
-                       Refer := x;
-                     end;
-      ttsVertical:  begin
-                       SlidingDimension := Height;
-                       Refer := y;
-                     end;
+    ttsHorizontal: begin
+      SlidingDimension := Width;
+      Refer := x;
     end;
+    ttsVertical: begin
+      SlidingDimension := Height;
+      Refer := y;
+    end;
+  end;
 
   if Refer < 0 then
-     Refer := 0;
+    Refer := 0;
 
-  tmppos := Round(Refer / ( SlidingDimension / (fmax - fmin)));
+  tmppos := Round(Refer / (SlidingDimension / (fmax - fmin)));
 
   if tmppos < fmin then
-     tmppos:=FMin;
+    tmppos := FMin;
   if tmppos > FMax then
-     tmppos:=FMax;
+    tmppos := FMax;
 
   if FOrientation = ttsVertical then
-     tmppos := FMax - tmppos;
+    tmppos := FMax - tmppos;
 
   fPosition := tmppos;
   RedrawControl;
 
 end;
 
-procedure TThemedSlider.MouseMove(Shift: TShiftState; X, Y: Integer);
+procedure TThemedSlider.MouseMove(Shift: TShiftState; X, Y: integer);
 begin
 
-  if (ssLeft in Shift) and IsEnabled  then
-    begin
-      fseeking := true;
-      case FOrientation of
-          ttsHorizontal: Cursor := crHSplit;
-          ttsVertical:  Cursor := crVSplit;
-        end;
-      Seek(X, Y);
-      if Assigned(FOnChange) then
-         FOnChange(self);
-      end
-  else
-    begin
-      fseeking := False;
-      Cursor := crDefault;
+  if (ssLeft in Shift) and IsEnabled then
+  begin
+    fseeking := True;
+    case FOrientation of
+      ttsHorizontal: Cursor := crHSplit;
+      ttsVertical: Cursor   := crVSplit;
     end;
+    Seek(X, Y);
+    if Assigned(FOnChange) then
+      FOnChange(self);
+  end
+  else
+  begin
+    fseeking := False;
+    Cursor   := crDefault;
+  end;
 
   inherited MouseMove(Shift, X, Y);
 end;
 
 procedure TThemedSlider.MouseDown(Button: TMouseButton; Shift: TShiftState; X,
-  Y: Integer);
+  Y: integer);
 begin
   if (Button = mbLeft) and IsEnabled then
-     fSeeking:=true;
+    fSeeking := True;
   RedrawControl;
   inherited MouseDown(Button, Shift, X, Y);
 end;
 
 procedure TThemedSlider.MouseUp(Button: TMouseButton; Shift: TShiftState; X,
-  Y: Integer);
+  Y: integer);
 begin
   fSeeking := False;
   RedrawControl;
@@ -326,43 +335,50 @@ begin
     exit;
 
   p := Mouse.CursorPos;
-  P:= ScreenToClient(p);
-  fSeeking:=true;
+  P := ScreenToClient(p);
+  fSeeking := True;
   Seek(p.x, p.y);
-  fSeeking:=false;
+  fSeeking := False;
   inherited Click;
+end;
+
+procedure TThemedSlider.Repaint;
+begin
+  inherited Repaint;
+  RedrawControl;
+
 end;
 
 constructor TThemedSlider.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
   ControlStyle := ControlStyle + [csCaptureMouse];
-  Color:= clBtnFace;
-  FMax:=100;
-  FMin:=0;
-  FPosition:=50;
-  Width:=200;
-  Height:=50;
-  InternalBitmap:= TBitmap.Create;
-  InternalBitmap.Height:=Height;
-  InternalBitmap.Width:= Width;
-  BorderWidth:=2;
-  FSliderwidth:= 6;
-  FOrientation:= ttsHorizontal;
-  fseeking:=false;
+  Color     := clBtnFace;
+  FMax      := 100;
+  FMin      := 0;
+  FPosition := 50;
+  Width     := 200;
+  Height    := 50;
+  InternalBitmap := TBitmap.Create;
+  InternalBitmap.Height := Height;
+  InternalBitmap.Width := Width;
+  BorderWidth := 2;
+  FSliderwidth := 6;
+  FOrientation := ttsHorizontal;
+  fseeking  := False;
 
   RedrawControl;
 end;
 
 destructor TThemedSlider.Destroy;
 begin
-  InternalBitmap.free;
+  InternalBitmap.Free;
   inherited Destroy;
 end;
 
 procedure TThemedSlider.EraseBackground(DC: HDC);
 begin
-//  inherited EraseBackground(DC);
+  //  inherited EraseBackground(DC);
 end;
 
 end.
--- src/uequalizer.pas
+++ src/uequalizer.pas
@@ -25,7 +25,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ButtonPanel,
-  ExtCtrls, StdCtrls, Buttons, equalizerband, AudioEngine, Equalizer, uDM,  Generics.Collections;
+  ExtCtrls, StdCtrls, Buttons, equalizerband, AudioEngine, Equalizer, uDM, Generics.Collections;
 
 type
 
@@ -41,7 +41,7 @@ type
     pnlContainer: TPanel;
     bSavePreset: TSpeedButton;
     bRemovePreset: TSpeedButton;
-    StaticText1: TStaticText;
+    stPreset: TStaticText;
     procedure bSavePresetClick(Sender: TObject);
     procedure cbEnableEqClick(Sender: TObject);
     procedure cbPresetChange(Sender: TObject);
@@ -52,9 +52,9 @@ type
     Eq: IEqualizer;
     EQBandList: TEQBandList;
     CurrentSettings: RPreset;
-    procedure BandChanged(Sender: TObject; const BandNo:integer; const Value: single);
+    procedure BandChanged(Sender: TObject; const BandNo: integer; const Value: single);
     procedure InitBands(BandInfo: ARBandinfo);
-    procedure PresetToScreen(Preset: Integer); overload;
+    procedure PresetToScreen(Preset: integer); overload;
     procedure PresetToScreen(Preset: RPreset); overload;
   public
 
@@ -64,47 +64,52 @@ var
   fEqualizer: TfEqualizer;
 
 implementation
+
 uses GUIBackEnd, AppConsts;
 
-{$R *.lfm}
+  {$R *.lfm}
 
-{ TfEqualizer }
+  { TfEqualizer }
 
 procedure TfEqualizer.FormCreate(Sender: TObject);
 var
-  i:integer;
+  i: integer;
 begin
 
   if (not BackEnd.AudioEngine.SupportEQ) or
     not Supports(BackEnd.AudioEngine, IEqualizer, Eq) then
-    begin
-      lbMessage.Caption := rUnsupportedEngine;
-      lbMessage.Visible:=True;
-      pnlContainer.Enabled:=false;
-      pnlHeader.Enabled:=false;
-      exit;
-    end;
+  begin
+    lbMessage.Caption    := rUnsupportedEngine;
+    lbMessage.Visible    := True;
+    pnlContainer.Enabled := False;
+    pnlHeader.Enabled    := False;
+    exit;
+  end;
 
   for i := 0 to pred(BackEnd.EqualizerParam.Count) do
-    begin
-      cbPreset.Items.Add(BackEnd.EqualizerParam[i].Name);
-    end;
+    cbPreset.Items.Add(BackEnd.EqualizerParam[i].Name);
 
-  EQBandList:= TEQBandList.Create(true);
+  EQBandList := TEQBandList.Create(True);
 
-  if Eq.ActiveEQ then
-    begin
-      InitBands(eq.BandInfo);
-    end;
+  cbEnableEq.Checked := BackEnd.EngineParam.ActiveEQ;
+  if not BackEnd.EngineParam.ActiveEQ then
+  begin
+    pnlContainer.Enabled  := False;
+    cbPreset.Enabled      := False;
+    bSavePreset.Enabled   := False;
+    bRemovePreset.Enabled := False;
+  end;
 
-  cbEnableEq.Checked:= BackEnd.EngineParam.ActiveEQ;
-  cbPreset.ItemIndex:= BackEnd.EngineParam.EQPreset;
+  if (EQBandList.Count = 0) then
+    InitBands(eq.BandInfo);
 
-  ButtonPanel1.OKButton.Images := DM.ilButtons;
+  cbPreset.ItemIndex := BackEnd.EngineParam.EQPreset;
+  cbPresetChange(cbPreset);
+
+  ButtonPanel1.OKButton.Images     := DM.ilButtons;
   ButtonPanel1.CancelButton.Images := DM.ilButtons;
   ButtonPanel1.OKButton.ImageIndex := 25;
   ButtonPanel1.CancelButton.ImageIndex := 26;
-
 
 end;
 
@@ -113,21 +118,21 @@ begin
   EQBandList.Free;
 end;
 
-Procedure TfEqualizer.InitBands( BandInfo: ARBandinfo);
+procedure TfEqualizer.InitBands(BandInfo: ARBandinfo);
 var
   i: integer;
   EQBand: TfrEqualizer;
 begin
   pnlContainer.DisableAlign;
-  for i := 0 to length(BandInfo)-1 do
+  for i := 0 to length(BandInfo) - 1 do
   begin
-    EQBand:= TfrEqualizer.Create(pnlContainer);
+    EQBand := TfrEqualizer.Create(pnlContainer);
     EQBandList.Add(EQBand);
-    EQBand.Name:='eq'+IntToStr(i);
-    EQBand.Parent:= pnlContainer;
-    EQBand.OnBandChanged:=@BandChanged;
-    EQBand.Align := alLeft;
-    EQBand.Left := i* EQBand.Width;
+    EQBand.Name   := 'eq' + IntToStr(i);
+    EQBand.Parent := pnlContainer;
+    EQBand.OnBandChanged := @BandChanged;
+    EQBand.Align  := alLeft;
+    EQBand.Left   := i * EQBand.Width;
     // Some engines allow a range greater than -12/+12 db, this should be supported by all engines...
     EQBand.InitBand(i, FloatToStr(BandInfo[i].freq), BandInfo[i].Value, -12, +12);
   end;
@@ -138,14 +143,24 @@ end;
 procedure TfEqualizer.cbEnableEqClick(Sender: TObject);
 var
   BandInfo: ARBandinfo;
+  I: Integer;
 begin
-  Eq.ActiveEQ:=cbEnableEq.Checked;
+  Eq.ActiveEQ := cbEnableEq.Checked;
+
+  pnlContainer.Enabled  := Eq.ActiveEQ;
+  cbPreset.Enabled      := Eq.ActiveEQ;
+  bSavePreset.Enabled   := Eq.ActiveEQ;
+  bRemovePreset.Enabled := Eq.ActiveEQ;
 
   BackEnd.EngineParam.ActiveEQ := Eq.ActiveEQ;
   if Eq.getActiveEQ and (EQBandList.Count = 0) then
-    begin
-      InitBands(eq.BandInfo);
-    end;
+    InitBands(eq.BandInfo);
+
+  For I:= 0 to EQBandList.count -1 do
+    EQBandList[i].slValue.Repaint;
+
+  pnlContainer.Repaint;
+
 end;
 
 procedure TfEqualizer.bSavePresetClick(Sender: TObject);
@@ -155,74 +170,66 @@ var
   i: integer;
 begin
   NewName := rDefaultPresetName;
-  if InputQuery(rAddEQualizerPreset, rNewEqualizerName,newName) then
-    begin
-      CurrentSettings.Name:=NewName;
-      idx := BackEnd.EqualizerParam.AddPreset(CurrentSettings);
-      cbPreset.Clear;
-      for i := 0 to pred(BackEnd.EqualizerParam.Count) do
-        begin
-          cbPreset.Items.Add(BackEnd.EqualizerParam[i].Name);
-        end;
-      PresetToScreen(CurrentSettings);
-      BackEnd.EngineParam.EQPreset:=idx;
-    end;
+  if InputQuery(rAddEQualizerPreset, rNewEqualizerName, newName) then
+  begin
+    CurrentSettings.Name := NewName;
+    idx := BackEnd.EqualizerParam.AddPreset(CurrentSettings);
+    cbPreset.Clear;
+    for i := 0 to pred(BackEnd.EqualizerParam.Count) do
+      cbPreset.Items.Add(BackEnd.EqualizerParam[i].Name);
+    PresetToScreen(CurrentSettings);
+    BackEnd.EngineParam.EQPreset := idx;
+  end;
 end;
 
 procedure TfEqualizer.cbPresetChange(Sender: TObject);
-
 begin
   CurrentSettings := BackEnd.EqualizerParam[cbPreset.ItemIndex];
   PresetToScreen(CurrentSettings);
   ApplyPreset(eq, CurrentSettings);
-  BackEnd.EngineParam.EQPreset:= cbPreset.ItemIndex;
+  BackEnd.EngineParam.EQPreset := cbPreset.ItemIndex;
 
 end;
 
 procedure TfEqualizer.cbPresetKeyPress(Sender: TObject; var Key: char);
 begin
-  key:=#00;
+  key := #00;
 end;
 
 
-Procedure TfEqualizer.PresetToScreen(Preset:RPreset);
+procedure TfEqualizer.PresetToScreen(Preset: RPreset);
 var
- i: integer;
+  i: integer;
 begin
- cbPreset.Caption:= Preset.Name;
- for i := 0 to pred(EQCounter) do
-   begin
-     EQBandList[i].Value := Preset.Values[i];
-   end;
+  cbPreset.Caption := Preset.Name;
+  for i := 0 to pred(EQCounter) do
+    EQBandList[i].Value := Preset.Values[i];
 end;
 
-Procedure TfEqualizer.PresetToScreen(Preset:Integer);
+procedure TfEqualizer.PresetToScreen(Preset: integer);
 var
- i: integer;
+  i: integer;
 begin
- cbPreset.Caption:= BackEnd.EqualizerParam[Preset].Name;
- for i := 0 to pred(EQCounter) do
-   begin
-     EQBandList[i].Value := BackEnd.EqualizerParam[Preset].Values[i];
-   end;
+  cbPreset.Caption := BackEnd.EqualizerParam[Preset].Name;
+  for i := 0 to pred(EQCounter) do
+    EQBandList[i].Value := BackEnd.EqualizerParam[Preset].Values[i];
 end;
 
 procedure TfEqualizer.BandChanged(Sender: TObject; const BandNo: integer;
   const Value: single);
 begin
   if not CurrentSettings.Modified then
-    begin
-      CurrentSettings.Modified:=true;
-      CurrentSettings.Name:=rDefaultPresetName;
-      cbPreset.Caption:=CurrentSettings.Name;
-    end;
-  CurrentSettings.Values[BandNo]:= Value;
+  begin
+    CurrentSettings.Modified := True;
+    CurrentSettings.Name := rDefaultPresetName;
+    cbPreset.Caption := CurrentSettings.Name;
+  end;
+  CurrentSettings.Values[BandNo] := Value;
   if Eq.ActiveEQ then
-    begin
-      eq.BandValue[BandNo]:= Value;
-      eq.EQApply;
-    end;
+  begin
+    eq.BandValue[BandNo] := Value;
+    eq.EQApply;
+  end;
 end;
 
 end.
-
